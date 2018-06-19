db8799007786e8b2f72258149f590dedf6d3bb68
ksmdeepak <deepakbunny007@gmail.com>
2018-06-19 13:30:17 -0600

    solved

---
+++ Merge_Two_Binary_Trees_617.java
@@ -0,0 +1,39 @@
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+class Merge_Two_Binary_Trees_617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if(t1!=null && t2!=null){
+            t1.val=t1.val+t2.val;
+        }
+        else if(t1==null && t2!=null){
+            t1= new TreeNode(t2.val);
+            t1.left = t2.left;
+            t1.right = t2.right;
+            t2.val=0;
+            t2.left=null;
+            t2.right=null;
+
+
+        }
+        else if(t1!=null && t2==null){
+            t2 = new TreeNode(0);
+            t2.left=null;
+            t2.right=null;
+        }
+        else{
+            return null;
+        }
+        t1.left=mergeTrees(t1.left,t2.left);
+        t1.right=mergeTrees(t1.right,t2.right);
+
+        return t1;
+
+    }
+}
---
+++ Trim_BST_669.java
@@ -0,0 +1,39 @@
+
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+public class Trim_BST_669 {
+
+
+        public boolean checkBoundaryConditions(int val, int L,int R){
+            if(val>=L && val<=R)
+                return true;
+            else
+                return false;
+        }
+        public TreeNode trimBST(TreeNode root, int L, int R) {
+            if(root==null) return null;
+
+            root.left = trimBST(root.left,L,R);
+            root.right = trimBST(root.right,L,R);
+
+            if(checkBoundaryConditions(root.val,L,R)){
+                return root;
+            }
+
+            else{
+                if(root.val<=R && root.right!=null && checkBoundaryConditions(root.right.val,L,R) )
+                    return root.right;
+                else if(root.val>=L && root.left!=null && checkBoundaryConditions(root.left.val,L,R) )
+                    return root.left;
+                else
+                    return null;
+            }
+        }
+
+}
