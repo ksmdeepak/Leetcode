334ad021abccccd5e89e17f899b61c995a0a9883
ksmdeepak <deepakbunny007@gmail.com>
2018-09-13 19:34:10 -0600

    more optimized solution

--- Python Solutions/Permutations2_47.py
+++ Python Solutions/Permutations2_47.py
@@ -1,14 +1,21 @@
 class Solution:
+    def check(self,nums,start,current):
+        i = start
+        while i<current:
+            if nums[i]==nums[current]:
+                return False
+            i+=1
+        return True
     def getPermutations(self,nums,l,r,output):
         if l==r:
             temp = nums.copy()
-            if temp not in output:
-                output.append(temp)
+            output.append(temp)
         else:
             for i in range(l,r+1):
-                nums[l],nums[i] = nums[i],nums[l]
-                self.getPermutations(nums,l+1,r,output)
-                nums[l],nums[i] = nums[i],nums[l]
+                if self.check(nums,l,i):
+                    nums[l],nums[i] = nums[i],nums[l]
+                    self.getPermutations(nums,l+1,r,output)
+                    nums[l],nums[i] = nums[i],nums[l]
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
@@ -16,5 +23,5 @@ class Solution:
         """
         output=[]
         self.getPermutations(nums,0,len(nums)-1,output)
-        return output
+        return list(output)
         
